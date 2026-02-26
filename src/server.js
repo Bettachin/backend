2ecd9328d57fccefa79e53258fbb8f787143cc58
Bettachin <zedricabejuela.30@gmail.com>
2026-02-26 17:35:54 +0800

    third commit

--- src/server.js
+++ src/server.js
@@ -5,8 +5,11 @@ const { startH02Server } = require("./tcp/h02Server");
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("HTTP API running on", PORT));
+const PORT = Number(process.env.PORT || 5000);
 
-// TCP Port for tracker
-startH02Server({ port: Number(process.env.TRACKER_TCP_PORT || 9000) });
+// HTTP API
+app.listen(PORT, "0.0.0.0", () => console.log("HTTP API running on", PORT));
+
+// TCP tracker (internal by default unless you enable TCP Proxy)
+const TCP_PORT = Number(process.env.TRACKER_TCP_PORT || 9000);
+startH02Server({ host: "0.0.0.0", port: TCP_PORT });
