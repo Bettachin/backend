a01ee3a69a959504e96c8bca1243c8578258c0f4
Bettachin <zedricabejuela.30@gmail.com>
2026-03-09 22:43:31 +0800

    srvr2

--- src/server.js
+++ src/server.js
@@ -5,16 +5,17 @@ const { startH02Server } = require("./tcp/h02Server");
 
 connectDB();
 
-const HTTP_PORT = process.env.PORT || 8080;
-const TCP_PORT = process.env.TRACKER_TCP_PORT || 9000;
+const HTTP_PORT = Number(process.env.PORT || 8080);
 
-// HTTP API (Railway public port)
 app.listen(HTTP_PORT, "0.0.0.0", () => {
   console.log("HTTP API running on", HTTP_PORT);
 });
 
-// TCP GPS tracker (internal port)
-startH02Server({
-  host: "0.0.0.0",
-  port: TCP_PORT,
-});
+// Disable TCP unless explicitly enabled
+if (process.env.ENABLE_TCP_TRACKER === "true") {
+  const TCP_PORT = Number(process.env.TRACKER_TCP_PORT || 9000);
+  startH02Server({ host: "0.0.0.0", port: TCP_PORT });
+  console.log("[TCP] H02 server started on", TCP_PORT);
+} else {
+  console.log("[TCP] Disabled");
+}
