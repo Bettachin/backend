501585f2c0f3f6dfffeadabc78e400b264cdca09
Bettachin <zedricabejuela.30@gmail.com>
2026-03-11 16:26:45 +0800

    g

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 
 const app = express();
 
-// safest demo CORS setup
 app.use((req, res, next) => {
   const origin = req.headers.origin || "*";
   res.header("Access-Control-Allow-Origin", origin);
